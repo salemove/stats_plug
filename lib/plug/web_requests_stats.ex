73d2c7f0df0a29ab7942d12af4d90fa5d689afbf
Gianluca Ciccarelli <gianluca.ciccarelli@glia.com>
2021-01-25 15:35:10 +0200

    Change the way the elapsed time is shown

--- lib/plug/web_requests_stats.ex
+++ lib/plug/web_requests_stats.ex
@@ -46,7 +46,7 @@ defmodule WebRequestsStats do
       conn,
       fn conn ->
         stop_time = System.monotonic_time()
-        elapsed_ms = System.convert_time_unit(stop_time - start_time, :native, :millisecond)
+        elapsed_ms = native_to_milliseconds(stop_time - start_time)
 
         if processed_by_phoenix?(conn) do
           send_metric(conn, elapsed_ms, opts)
@@ -98,5 +98,12 @@ defmodule WebRequestsStats do
 
   defp processed_by_phoenix?(conn) do
     conn.private[:phoenix_controller] && conn.private[:phoenix_action]
+  end
+
+  defp native_to_milliseconds(duration) do
+    # convert_time_unit rounds the number using the floor function. To get
+    # milliseconds with 3 decimal points we convert the duration to
+    # microseconds and then divide it by a thousand.
+    System.convert_time_unit(duration, :native, :microsecond) / 1000
   end
 end
